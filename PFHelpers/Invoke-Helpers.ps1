1acecfd0803d4d441bcabdbdec5473a557ce6def
Gavin Jones <gjones@powerfarming.co.nz>
2020-05-20 09:52:17 +1200

    Add some basic admin helpers

--- PFHelpers/Invoke-Helpers.ps1
+++ PFHelpers/Invoke-Helpers.ps1
@@ -1,3 +1,27 @@
 function Invoke-Chrome {
     & "${env:ProgramFiles(x86)}\Google\Chrome\Application\chrome.exe"
 }
+
+function Get-CurrentPrincipal {
+    return [Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()
+}
+
+function Test-AmIAdmin {
+    return ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')
+}
+
+function Invoke-SelfElevate {
+    # Self-elevate the script if required
+    if (-Not (Test-AmIAdmin)) {
+    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+      $CommandLine = " "
+      if($MyInvocation.MyCommand.Path) {
+          $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+      }
+      Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+    }
+   } else {
+      Start-Process -FilePath PowerShell.exe
+      # Read-Host 'Press Enter to continue…' | Out-Null
+   }
+}
